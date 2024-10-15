372c1d5a71349c613b15cb32c83d4be1463a1092
Diego Alonzo Gutierrez <diego.alonzo9090@gmail.com>
2024-10-15 13:54:42 -0500

    Solucion Conflito Carga Menu

--- menu_principal.py
+++ menu_principal.py
@@ -7,15 +7,13 @@ def cargar_menu_principal():
     window.title("Menú Principal - Sistema de Gestión")
 
     window.geometry("600x400")
-    window.configure(bg="lightgreen")
+    window.configure(bg="lightblue")
 
     menu_bar = tk.Menu(window)
 
     window.config(menu=menu_bar)
 
-    label_bienvenida = tk.Label(window, text="Bienvenido al Sistema de Gestión", font=("Arial", 16), bg="lightgreen")
+    label_bienvenida = tk.Label(window, text="Bienvenido al Sistema de Gestión", font=("Arial", 16), bg="lightblue")
     label_bienvenida.pack(pady=50)
 
     window.mainloop()
-
-cargar_menu_principal()
